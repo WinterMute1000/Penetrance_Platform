05adabfb72e2992a86de0b1e521dd4a1a7eedf8d
WinterMute1000 <realistic20@naver.com>
2021-05-17 11:50:50 +0900

    XSS model create

--- penetrance_platform/xss/models.py
+++ penetrance_platform/xss/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class XSSCode(models.Model):
+    xss_code = models.CharField(max_length=2048)
+
+    objects = models.Manager()
+
+
+class XSSLog(models.Model):
+    host = models.CharField(max_length=2048)
+    scan_date = models.DateTimeField(auto_now=True)
+    xss_code = models.CharField(max_length=2048)
+    result = models.BooleanField()
+
+    objects = models.Manager()
